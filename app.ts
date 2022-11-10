6d3834fecd7e38c7e92c4e10aab30cfa5b46a059
FernasG <fernandogoul7@gmail.com>
2022-11-10 01:21:13 -0300

    refactor: move view files and handlebars helpers to a public folder in root path

--- app.ts
+++ app.ts
@@ -1,18 +1,18 @@
 import express, { Express } from "express";
 import { engine } from "express-handlebars";
 import { RequestMiddleware } from "@middleware"
-import { Helpers } from "./src/handlebars/HandlebarsHelpers";
+import { Helpers } from "@handlebars-helpers";
 import router from "@routes";
 import path from "path";
 
 const setupServer = ((app: Express) => {
     app.use(express.json());
     app.use(RequestMiddleware.request);
-    app.use(express.static(path.resolve('src', 'views', 'layouts')));
+    app.use(express.static(path.resolve('public', 'views', 'layouts')));
     app.use(router);
 
     app.set('view engine', 'hbs');
-    app.set('views', path.resolve('src', 'views'));
+    app.set('views', path.resolve('public', 'views'));
     app.engine('hbs', engine({ defaultLayout: 'index.hbs', helpers: Helpers }));
 });
 
